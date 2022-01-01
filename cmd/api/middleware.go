f298cf4af8cc95b6aa446d27950b2d4dd2723f8b
Ally Maumba <amaumba1@gmail.com>
2022-01-01 15:12:01 -0600

    allow all the request * to resolve the CORS issue

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -5,8 +5,6 @@ import "net/http"
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		next.ServeHTTP(w, r)
 	})
 }
